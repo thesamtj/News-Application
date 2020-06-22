562bdfd4e9bf01a741a529af87b68a8b1babec0b
Samuel Tijani <omalitijani@gmail.com>
2020-06-22 13:25:38 +0100

    Load articles and sources

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsApiService } from 'src/app/news-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  mArticles: Array<any>;
+  mSources: Array<any>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private newsapi: NewsApiService) {
+    console.log('app component constructor called');
   }
 
+  ngOnInit() {
+    //load articles
+    this.newsapi
+      .initArticles()
+      .subscribe((data) => (this.mArticles = data['articles']));
+    //load news sources
+    this.newsapi
+      .initSources()
+      .subscribe((data) => (this.mSources = data['sources']));
+  }
+
+  searchArticles(source) {
+    console.log('selected source is: ' + source);
+    this.newsapi
+      .getArticlesByID(source)
+      .subscribe((data) => (this.mArticles = data['articles']));
+  }
 }
