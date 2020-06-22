6feb40b9433fd45cd59baf4a85181fc6124f9d56
Samuel Tijani <omalitijani@gmail.com>
2020-06-22 13:03:57 +0100

    Add news api

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
 
-  constructor() { }
-}
+  api_key = '99db21c64791433a9ab238a635b83e1f';
+
+  constructor(private http:HttpClient) { }
+  initSources(){
+     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+} 
